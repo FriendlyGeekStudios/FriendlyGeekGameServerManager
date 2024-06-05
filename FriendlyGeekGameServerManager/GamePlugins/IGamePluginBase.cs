63710308b6641f5cdd6a560dd441d93bc3278f1c
Evan Snyder <evansnyder16@gmail.com>
2024-06-05 15:28:03 -0400

    Added create button and plugin base class

---
+++ FriendlyGeekGameServerManager/GamePlugins/IGamePluginBase.cs
@@ -0,0 +1,11 @@
+﻿namespace FriendlyGeekGameServerManager.GamePlugins;
+
+public interface IGamePluginBase
+{
+    string GameName { get; }
+    string? SteamAppID { get; }
+
+    int CreateServer();
+    int StartServer();
+    int StopServer();
+}
