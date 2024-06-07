43677a3eb20b899f1a34eb7508ca17e709443ec2
Jason Moran <jm.gobills@gmail.com>
2024-06-06 19:47:24 -0500

    push it real good

---
+++ FriendlyGeekGameServerManager/Models/Server.cs
@@ -0,0 +1,11 @@
+﻿namespace FriendlyGeekGameServerManager.Models
+{
+    public class Server
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public ServerStatus Status { get; set; }
+        public string? GamePort { get; set; }
+        public string? QueryPort { get; set; }
+    }
+}
